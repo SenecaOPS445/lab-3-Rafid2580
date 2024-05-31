fa761cc44c1bc11d3df74488c85fb06af4e6e646
Rafid Al Fahim <rfahim1@myseneca.ca>
2024-05-31 02:19:10 -0400

    Lab3a.py done

---
+++ lab3a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# return_text_value() function
+# Author ID: 186140216
+
+def return_text_value():
+
+    return "Good Morning Terry"
+
+# return_number_value() function
+
+def return_number_value():
+
+    num_1 = 6
+    num_2 = 9
+    return num_1 + num_2
+
+
+
+# Main Program
+
+
+if __name__ == "__main__":
+    print("Python code")
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
