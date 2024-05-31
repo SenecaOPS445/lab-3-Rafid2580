42c1ecb0b3f79b631449c83ebe67daf0117a8b62
Rafid Al Fahim <rfahim1@myseneca.ca>
2024-05-31 14:36:09 -0400

    lab3c.py done

---
+++ lab3c.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Lab 3 Inv 2 function operate '''
+# Author ID: 181042616
+
+def operate(num1, num2, operator):
+    if operator == 'add':
+        return num1 + num2
+    elif operator == 'subtract':
+        return num1 - num2
+    elif operator == 'multiply':
+        return num1 * num2
+    else:
+        return 'Error: function operator can be "add", "subtract", or "multiply"'
+
+if __name__ == '__main__':
+    print(operate(10, 5, 'add'))
+    print(operate(10, 5, 'subtract'))
+    print(operate(10, 5, 'multiply'))
+    print(operate(10, 5, 'divide'))
+
